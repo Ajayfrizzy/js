bf6a3515ca88455ccafd4c101a5945216749c605
Ajayfrizzy <ajayholuwaseun@gmail.com>
2025-11-15 11:53:08 +0100

    Js

---
+++ index.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('.form'); 
+const fname = document.querySelector('.fname').value.trim();
+const lname = document.querySelector('.lname').value.trim();
+const email = document.querySelector('.email').value.trim();
+const number = document.querySelector('.pnumber').value.trim();
+
+//const info = document.querySelector(".information");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+
+    checkValidation();
+});
