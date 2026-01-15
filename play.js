b4e41d4e7b60c247edd3f3456870e76bd4eb0650
Ajayfrizzy <ajayholuwaseun@gmail.com>
2026-01-15 21:42:34 +0100

    Yoh

--- play.js
+++ play.js
@@ -204,9 +204,8 @@ func(
     () => {alert("You canceled the execution");}
 ); */
 
-let array = [65, 42, 78, 12, 5, 67, 90, 36];
+let array = [65, 42, 78, 12, 5, 67, 90, 36, 23, 39];
 
 array.sort(() => Math.random() - 0.5);
 
-console.log(arr);
-    
+console.log(array);
